b0afd9e1f3187b7baf5c39460334c3c9ab8ae91c
Kirill Abramov <kirillabramov@Kirills-MacBook-Pro.local>
2019-02-23 12:50:54 +0200

    add eslint, husky and prettier configs

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,13 +1,8 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-          React Starter
-      </div>
-    );
+    return <div className="App">React Starter</div>;
   }
 }
 
